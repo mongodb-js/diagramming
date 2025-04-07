13f63e753990e682ec3d1c2a4b623d941e805852
Lavender <lavender.chan@mongodb.com>
2025-04-07 12:11:24 +1000

    chore/MIG-6565 Upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintPlugin from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactPlugin from 'eslint-plugin-react';
+import testingLibraryPlugin from 'eslint-plugin-testing-library';
+import * as emotionPlugin from '@emotion/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parser: parser,
+    },
+    plugins: {
+      prettier: prettierPlugin,
+      react: reactPlugin,
+      emotion: emotionPlugin,
+      import: importPlugin,
+      '@typescript-eslint': eslintPlugin,
+      'react-hooks': reactHooksPlugin,
+      'testing-library': testingLibraryPlugin,
+    },
+    rules: {
+      ...testingLibraryPlugin.configs.react.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintPlugin.configs.stylistic,
+      'no-unexpected-multiline': 'error',
+      'no-console': 'error',
+      'prefer-arrow-callback': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+        },
+      ],
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+];
