4a8c12086893ac13a2262cd767a26290d0d9c00e
Lavender <lavender.chan@mongodb.com>
2025-04-07 16:07:20 +1000

    feature/MIG-6568 Add missing types

--- src/components/node/node.stories.tsx
+++ src/components/node/node.stories.tsx
@@ -1,10 +1,6 @@
-import { Meta, ReactRenderer, StoryObj } from '@storybook/react';
-import { ThemeProvider } from '@emotion/react';
-import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { Meta, StoryObj } from '@storybook/react';
 
-import { LIGHT_THEME } from '@/styles/theme-light';
 import { InternalNode } from '@/types/internal';
-import { DARK_THEME } from '@/styles/theme-dark';
 import { Node } from '@/components/node/node';
 
 const INTERNAL_NODE: InternalNode = {
