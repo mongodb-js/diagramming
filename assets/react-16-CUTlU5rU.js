19f6d2ca02399d34c96b79edee12ccf1a68651a5
lchans <lchans@users.noreply.github.com>
2025-06-16 23:38:17 +0000

    deploy: 857da58545bf6f3ce33aab405cd7edd43f51ce2b

---
+++ assets/react-16-CUTlU5rU.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-lyVYDOz6.js";import"./iframe-BsTjVIyr.js";var p=async(e,r)=>new Promise(o=>{n.render(e,r,()=>o(null))}),u=e=>{n.unmountComponentAtNode(e)};export{p as renderElement,u as unmountElement};
