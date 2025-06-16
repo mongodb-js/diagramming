857da58545bf6f3ce33aab405cd7edd43f51ce2b
Lavender <lavender.chan@mongodb.com>
2025-06-17 09:37:36 +1000

    chore/MIG-6938 Upgrade Storybook and associated packages (#70)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,12 +13,7 @@ function getAbsolutePath(value: string): any {
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-themes'),
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-interactions'),
-  ],
+  addons: [getAbsolutePath('@storybook/addon-themes')],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
