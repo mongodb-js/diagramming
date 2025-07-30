36d490bd1d679d596c928aa21dac2d1f7ab220bf
gribnoysup <gribnoysup@users.noreply.github.com>
2025-07-30 09:22:45 +0000

    deploy: b9dc9eeea9219e8a6182e3a70dbfd0d1a9152a5c

---
+++ assets/react-16-ClFkQ7lJ.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-BtUnCeQ6.js";import"./iframe-DcTZrlbL.js";var p=async(e,r)=>new Promise(o=>{n.render(e,r,()=>o(null))}),u=e=>{n.unmountComponentAtNode(e)};export{p as renderElement,u as unmountElement};
