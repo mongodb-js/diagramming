41938e8f17f58c62c43f7711d1b8d452b4f33bd0
Paula Stachova <paula.stachova@mongodb.com>
2025-08-06 11:58:40 +0200

    update edge tests

--- src/components/edge/floating-edge.test.tsx
+++ src/components/edge/floating-edge.test.tsx
@@ -1,9 +1,10 @@
-import { Position, useNodes } from '@xyflow/react';
+import { Node, Position, useNodes } from '@xyflow/react';
 import { ComponentProps } from 'react';
 
 import { EMPLOYEES_NODE, ORDERS_NODE } from '@/mocks/datasets/nodes';
 import { render, screen } from '@/mocks/testing-utils';
 import { FloatingEdge } from '@/components/edge/floating-edge';
+import { DEFAULT_FIELD_HEIGHT, DEFAULT_NODE_WIDTH } from '@/utilities/constants';
 
 vi.mock('@xyflow/react', async () => {
   const actual = await vi.importActual<typeof import('@xyflow/react')>('@xyflow/react');
@@ -13,14 +14,23 @@ vi.mock('@xyflow/react', async () => {
   };
 });
 
+function mockNodes(nodes: Node[]) {
+  const mockedNodes = vi.mocked(useNodes);
+  mockedNodes.mockReturnValue(nodes);
+}
+
 describe('floating-edge', () => {
+  const nodes = [
+    { ...ORDERS_NODE, data: { title: ORDERS_NODE.title, fields: ORDERS_NODE.fields } },
+    { ...EMPLOYEES_NODE, data: { title: EMPLOYEES_NODE.title, fields: EMPLOYEES_NODE.fields } },
+  ];
+
   beforeEach(() => {
-    const nodes = [
-      { ...ORDERS_NODE, data: { title: ORDERS_NODE.title, fields: ORDERS_NODE.fields } },
-      { ...EMPLOYEES_NODE, data: { title: EMPLOYEES_NODE.title, fields: EMPLOYEES_NODE.fields } },
-    ];
-    const mockedNodes = vi.mocked(useNodes);
-    mockedNodes.mockReturnValue(nodes);
+    mockNodes(nodes);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
   });
 
   const renderComponent = (props?: Partial<ComponentProps<typeof FloatingEdge>>) => {
@@ -40,14 +50,44 @@ describe('floating-edge', () => {
     );
   };
 
-  it('Should render edge', () => {
-    renderComponent();
-    const path = screen.getByTestId('floating-edge-orders-to-employees');
-    expect(path).toHaveAttribute('id', 'orders-to-employees');
-    expect(path).toHaveAttribute(
-      'd',
-      'M240 143.5L240 163.5L 240,213Q 240,218 245,218L 381,218Q 386,218 386,223L386 272.5L386 292.5',
-    );
+  describe('Without measured heights', () => {
+    it('Should render edge', () => {
+      renderComponent();
+      const path = screen.getByTestId('floating-edge-orders-to-employees');
+      expect(path).toHaveAttribute('id', 'orders-to-employees');
+      expect(path).toHaveAttribute(
+        'd',
+        'M269.5 180L289.5 180L 386.5,180Q 391.5,180 391.5,185L 391.5,295Q 391.5,300 386.5,300L371.5 300',
+      );
+    });
+  });
+
+  describe('With measured heights', () => {
+    it('Should render edge', () => {
+      mockNodes([
+        {
+          ...nodes[0],
+          measured: {
+            width: DEFAULT_NODE_WIDTH,
+            height: DEFAULT_FIELD_HEIGHT * 2,
+          },
+        },
+        {
+          ...nodes[1],
+          measured: {
+            width: DEFAULT_NODE_WIDTH,
+            height: DEFAULT_FIELD_HEIGHT * 4,
+          },
+        },
+      ]);
+      renderComponent();
+      const path = screen.getByTestId('floating-edge-orders-to-employees');
+      expect(path).toHaveAttribute('id', 'orders-to-employees');
+      expect(path).toHaveAttribute(
+        'd',
+        'M240 143.5L240 163.5L 240,213Q 240,218 245,218L 381,218Q 386,218 386,223L386 272.5L386 292.5',
+      );
+    });
   });
 
   it('Should not render edge if source does not exist', () => {
--- src/utilities/get-edge-params.test.ts
+++ src/utilities/get-edge-params.test.ts
@@ -1,19 +1,53 @@
 import { getEdgeParams } from '@/utilities/get-edge-params';
 import { EMPLOYEES_NODE, ORDERS_NODE } from '@/mocks/datasets/nodes';
+import { DEFAULT_FIELD_HEIGHT, DEFAULT_NODE_WIDTH } from '@/utilities/constants';
 
 describe('get-edge-params', () => {
-  it('Should get parameters', () => {
-    const result = getEdgeParams(
-      { ...ORDERS_NODE, data: { title: ORDERS_NODE.title, fields: ORDERS_NODE.fields } },
-      { ...EMPLOYEES_NODE, data: { title: EMPLOYEES_NODE.title, fields: EMPLOYEES_NODE.fields } },
-    );
-    expect(result).toEqual({
-      sourcePos: 'bottom',
-      sx: 240,
-      sy: 143.5,
-      targetPos: 'top',
-      tx: 386,
-      ty: 292.5,
+  describe('Without measured heights', () => {
+    it('Should get parameters', () => {
+      const result = getEdgeParams(
+        { ...ORDERS_NODE, data: { title: ORDERS_NODE.title, fields: ORDERS_NODE.fields } },
+        { ...EMPLOYEES_NODE, data: { title: EMPLOYEES_NODE.title, fields: EMPLOYEES_NODE.fields } },
+      );
+      expect(result).toEqual({
+        sourcePos: 'right',
+        sx: 269.5,
+        sy: 180,
+        targetPos: 'right',
+        tx: 371.5,
+        ty: 300,
+      });
+    });
+  });
+
+  describe('With measured heights', () => {
+    it('Should get parameters', () => {
+      const result = getEdgeParams(
+        {
+          ...ORDERS_NODE,
+          data: { title: ORDERS_NODE.title, fields: ORDERS_NODE.fields },
+          measured: {
+            width: DEFAULT_NODE_WIDTH,
+            height: DEFAULT_FIELD_HEIGHT * 2,
+          },
+        },
+        {
+          ...EMPLOYEES_NODE,
+          data: { title: EMPLOYEES_NODE.title, fields: EMPLOYEES_NODE.fields },
+          measured: {
+            width: DEFAULT_NODE_WIDTH,
+            height: DEFAULT_FIELD_HEIGHT * 4,
+          },
+        },
+      );
+      expect(result).toEqual({
+        sourcePos: 'bottom',
+        sx: 240,
+        sy: 143.5,
+        targetPos: 'top',
+        tx: 386,
+        ty: 292.5,
+      });
     });
   });
 });
